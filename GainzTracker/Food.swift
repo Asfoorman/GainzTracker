31c1101e8c7626e7b0c8fa228865baa855c7742a
Anes Mehai <nsamehai@gmail.com>
2018-06-26 15:18:59 -0400

    initial commit

---
+++ GainzTracker/Food.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  GainzTracker
+//
+//  Created by Anes Mehai  on 6/26/18.
+//  Copyright © 2018 Anes Mehai . All rights reserved.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var description: String   
+    
+}
---
+++ GainzTracker/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  GainzTracker
+//
+//  Created by Anes Mehai  on 6/26/18.
+//  Copyright © 2018 Anes Mehai . All rights reserved.
+//
+
+import Foundation
